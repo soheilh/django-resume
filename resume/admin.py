f7bdabab12d0657b86837bc0328baaece7169ac7
soheil <soheilh105@gmail.com>
2024-06-14 19:10:20 +0330

    Add ModelAdmin for education model

--- resume/admin.py
+++ resume/admin.py
@@ -5,6 +5,13 @@ from .models import *
 admin.site.register(Bio)
 admin.site.register(University)
 admin.site.register(Advisor)
-admin.site.register(Education)
+
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('degree', 'major', 'university', 'start', 'end',)
+    list_filter = ('degree', 'start', 'end',)
+    search_fields = ('major', 'university_title', 'thesis',)
+    date_hierarchy = 'start'
+
+admin.site.register(Education, EducationAdmin)
 admin.site.register(Publication)
 admin.site.register(PublicationLink)
