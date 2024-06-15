f35e889fdfddab7de2710b1f3a356300c577bba0
soheil <soheilh105@gmail.com>
2024-06-15 14:41:26 +0330

    feat: Update Education model with additional fields and functionality
    - Added `blank=True` for the `advisors` field to make it optional.
    - Added `thesis_score` field to store the score of the thesis.
    - Introduced `DEGREE_RANKS` dictionary and `degree_rank` field to sort educations based on degree.
    - Overrode the `save` method to automatically set the `degree_rank` based on the selected degree.
    
    chore: Create and apply migration for Education model changes

--- resume/models.py
+++ resume/models.py
@@ -34,8 +34,9 @@ class Education(models.Model):
     end = models.DateField()
     gpa = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
     gpa_base = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
-    advisors = models.ManyToManyField(Advisor, related_name='educations')
+    advisors = models.ManyToManyField(Advisor, related_name='educations', blank=True)
     thesis = models.CharField(max_length=255, blank=True)
+    thesis_score = models.CharField(max_length=4, blank=True)
     DEGREE_CHOICES = [
         ('DIP', 'Diploma'),
         ('BD', 'B.Sc.'),
@@ -43,8 +44,19 @@ class Education(models.Model):
         ('PHD', 'Ph.D.'),
         ('POSTDOC', 'Postdoctoral'),
     ]
+    DEGREE_RANKS = {
+        'DIP': 1,
+        'BD': 2,
+        'MD': 3,
+        'PHD': 4,
+        'POSTDOC': 5,
+    }
     degree = models.CharField(max_length=8, choices=DEGREE_CHOICES,default='DIP')
-    
+    degree_rank = models.PositiveIntegerField(editable=False, default=0)
+
+    def save(self, *args, **kwargs):
+        self.degree_rank = self.DEGREE_RANKS[self.degree]
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.major} at {self.university.title}"
