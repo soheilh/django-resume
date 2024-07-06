91fbc50df200838eec875cad0d3d3c6cab720e6d
soheil <soheilh105@gmail.com>
2024-07-06 20:29:14 +0330

    feat: Added `Category` and `Post` models to the blog app
    - Created `Category` and `Post` model.
    - Configured Django admin for `Category` and `Post` models with custom settings.
    - Applied database migrations to reflect schema changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register Post Model
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'slug', 'status', 'category_to_str',)
+	list_filter = ('publish', 'status',)
+	search_fields = ('title', 'description', 'content',)
+	prepopulated_fields = {'slug': ('title',)}
+	ordering = ('-status', '-publish',)
+	# exclude = ('author', )
+
+	def category_to_str(self, obj):
+		return [category for category in obj.category.all()]
+	category_to_str.short_description = 'Categories'
+
+# Register Category Model
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name', 'slug', 'status',)
+	list_filter = ('status',)
+	search_fields = ('name', )
+	prepopulated_fields = {'slug':('name',)}
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+# Model for Category
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+# Model for Post
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=False, blank=False)
+    # author = 
+    slug = models.SlugField(max_length=120, unique=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    category = models.ManyToManyField(to=Category, related_name="posts")
+    thumb = models.ImageField(upload_to ='thumbs', default='thumbs/default.jpg')
+    image = models.ImageField(upload_to ='images', default='images/default.jpg')
+    STATUS_CHOICES = [
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('a', 'Archived'),
+        ('pa', 'Pending Approval'),
+        ('d', 'Deleted'),
+    ]
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='p')
+    content = RichTextField(default='')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-publish']
+
